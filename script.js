691d0532a2d2d227dad328a91a384d5557f60b86
Mzach55 <mikezach37@gmail.com>
2023-01-06 11:31:14 -0500

    didnt use the script file yet

---
+++ script.js
@@ -0,0 +1,28 @@
+function getComputerChoice () {
+    // create an array with rock, paper, scizzors contained within it
+    // select one of those indexes randomly
+    // return the choosen value
+}
+
+let playerTurn;  // prompt user for an input of rock,paper,scizzors
+let computerTurn;  // call the getComputerChoice functiopn
+
+
+
+function playRound (playerTurn, computerTurn) {
+    // create system to determine which choice beats what
+    // return a string that declares a winner
+    // if playerTurn ==== computerTurn, game is a tie
+    // if playerTurn = rock, and computerTurn = paper, computer win
+        // else player win.
+    // if player turn = paper, and computer turn = sciz, computer win
+        // else player win
+    // if player turn = scizzor, and computer turn = rock, computer win
+        // else player win
+}
+
+function game () {
+    // call the playRound function
+    // console.log to display results at end of the round
+    // loop over this process until a winner counts hit 3
+}
