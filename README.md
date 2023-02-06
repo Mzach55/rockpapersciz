cb1538dffb9ba1820ab81d03f6097bdaef2fb343
Mzach55 <mikezach37@gmail.com>
2023-02-06 13:54:38 -0500

    added style + counter, needs to break at 3 wins

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rockpapersciz
 rock paper scizzors from scratch written in JS
+
+
+read me update as of 02/06/2023 commit
+
+I have the core functionality of the site working. The dom is working properly to append and style the elements
+the on click works on each image and the logical works well
+the player count updates correctly
+
+---------------------------
+problems remaining -
+
+1. Break the eventListener when player or bot hits a score of 3. For whatever reason, I can not figure this out
--- script.js
+++ script.js
@@ -1,28 +1,107 @@
+let playervic = 0;
+let botvic = 0;
+
 function getComputerChoice () {
     // create an array with rock, paper, scizzors contained within it
+
+    const choices = ["rock","paper","scizzors"];
+
     // select one of those indexes randomly
+
+    let option = choices[Math.floor(Math.random()*choices.length)];
+
+    return option
+
     // return the choosen value
 }
-
-let playerTurn;  // prompt user for an input of rock,paper,scizzors
-let computerTurn;  // call the getComputerChoice functiopn
-
-
 
 function playRound (playerTurn, computerTurn) {
     // create system to determine which choice beats what
     // return a string that declares a winner
-    // if playerTurn ==== computerTurn, game is a tie
-    // if playerTurn = rock, and computerTurn = paper, computer win
-        // else player win.
-    // if player turn = paper, and computer turn = sciz, computer win
-        // else player win
-    // if player turn = scizzor, and computer turn = rock, computer win
-        // else player win
+    // if playerTurn == computerTurn, game is a tie
+
+    // console.log('please work!')
+
+    if (playerTurn == 'rock' && computerTurn == 'scizzors')  {
+    //    console.log('The player won')
+        return playervic += 1
+    }
+        else if (playerTurn == 'paper' && computerTurn == 'rock') {
+            // console.log('The player won')
+            return playervic += 1
+        }
+        else if (playerTurn == 'scizzors' && computerTurn == 'paper') {
+            // console.log('The player won')
+            return playervic += 1
+        }
+
+        else if (computerTurn == 'paper' && playerTurn == 'rock') {
+            // console.log('The computer won')
+            return botvic += 1
+    }
+        else if (computerTurn == 'rock' && playerTurn == 'scizzors'){
+            // console.log('The computer won')
+            return botvic += 1
+        }
+        else if (computerTurn == 'scizzors' && playerTurn == 'paper') {
+            // console.log('The computer won')
+            return botvic += 1
+        }
+        else {
+            // console.log('The round is a tie.')
+        }
+    
+};
+
+function game (player) {
+
+
+    let playerTurn = player
+    let computerTurn = getComputerChoice();
+    playRound(playerTurn,computerTurn);
+
+    console.log(playervic);
+    console.log(botvic);
+    console.log(playerTurn);
+    console.log(computerTurn);
+
+    if (playervic == 3) {
+        const playerwin = document.createElement('h1');
+        playerwin.textContent = "The player was victorious! Death to the machines!"
+        document.body.appendChild(playerwin)
+    }
+    else if (botvic == 3) {
+        const botwin = document.createElement('h1');
+        botwin.textContent = "The computer prevails! Death to all humans!"
+        document.body.appendChild(botwin)
+    }
+    
+    pointcounter.textContent = (`Player; ${playervic} | Computer; ${botvic}`);
+    
+
 }
 
-function game () {
-    // call the playRound function
-    // console.log to display results at end of the round
-    // loop over this process until a winner counts hit 3
-}
+
+
+const btnrock = document.querySelector('.rock');
+btnrock.addEventListener('click', () => game('rock'));
+
+const btnpaper = document.querySelector('.paper');
+btnpaper.addEventListener('click', () => game('paper'));
+
+const btnscizz = document.querySelector('.scizzors');
+btnscizz.addEventListener('click', () => game('scizzors'));
+
+const pointcounter = document.createElement('h1');
+pointcounter.setAttribute('style', 'display: flex; justify-content: center;')
+
+document.body.appendChild(pointcounter);
+
+
+if (botvic == 3 || playervic == 3) {
+    btnrock.removeEventListener('click', () => game('rock'));
+    btnpaper.removeEventListener('click', () => game('paper'));
+    btnscizz.removeEventListener('click', () => game('scizzors'));
+};
+
+
